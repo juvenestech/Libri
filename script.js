4003f80983554d58ea57ed6d4cf279e4a74fc8b7
juvenestech <97456286+juvenestech@users.noreply.github.com>
2024-05-08 17:35:16 +0200

    Generazione dinamica

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const librerie = ["Libri Quinto Piano", "Classici", "Romanzo", "Lingue", "Avventura", "Romanzi Rosa", "Manuali", "Romanzi Di Formazione"]
 
-$(librerie).each(libreria =>
+$(librerie).each((i, libreria) =>
     fetch(`libri/${libreria}.json`)
     .then(file => file.json())
     .then(json => {
         const libri = json.results[0].result.formatted;
         for (let i=0; i<libri.length; i++){
             const l = libri[i];
-            
+
             $('<div>')
                 .addClass('libro')
                 .append($('<h1>').text(l["Titolo libro"]))
