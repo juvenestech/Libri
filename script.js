83f65e26156d034f8109ec6295e5f6ed0f079b3e
juvenestech <97456286+juvenestech@users.noreply.github.com>
2024-05-08 17:32:52 +0200

    generazione dinamica delle card

---
+++ script.js
@@ -0,0 +1,19 @@
+const librerie = ["Libri Quinto Piano", "Classici", "Romanzo", "Lingue", "Avventura", "Romanzi Rosa", "Manuali", "Romanzi Di Formazione"]
+
+$(librerie).each(libreria =>
+    fetch(`libri/${libreria}.json`)
+    .then(file => file.json())
+    .then(json => {
+        const libri = json.results[0].result.formatted;
+        for (let i=0; i<libri.length; i++){
+            const l = libri[i];
+            
+            $('<div>')
+                .addClass('libro')
+                .append($('<h1>').text(l["Titolo libro"]))
+                .append($('<h3>').text(l["Nome autore"] + " " + l["Cognome autore"]))
+                .append()
+                .appendTo('#libri');
+        }
+    })
+);
